26fc52de8a5baf5e4a9f80eb9a2bfd6d6a0886a6
Lotolim Philip Pkiach <philippkiach686@gmail.com>
2023-05-25 07:38:48 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
  * main - entry point
  * @ac: arg count
  * @av: arg vector
- * Return: 0 on success, 1 is error
+ *
+ * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
 {
@@ -15,9 +13,10 @@ int main(int ac, char **av)
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
@@ -28,17 +27,18 @@ int main(int ac, char **av)
 			if (errno == ENOENT)
 			{
 				_eputs(av[0]);
-				_eputs(": 0: cant open");
+				_eputs(": 0: Can't open ");
 				_eputs(av[1]);
 				_eputchar('\n');
-				_eputchar(BUFFER_FLUSH);
+				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		info->read = fd;
+		info->readfd = fd;
 	}
 	populate_env_list(info);
+	read_history(info);
 	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
