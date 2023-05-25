76608f0d2384bee8f83c65bfd757536613d22e5b
Lotolim Philip Pkiach <philippkiach686@gmail.com>
2023-05-25 08:17:34 +0300

    Update getline2.c

--- getline2.c
+++ getline2.c
@@ -1,24 +1,61 @@
 #include "main.h"
 
 /**
- * free_buffer - frees the memory allocated for getline
+ * _getline - gets the next line of input from STDIN
+ * @info: parameter struct
+ * @ptr: address of pointer to buffer, preallocated or NULL
+ * @length: size of preallocated ptr buffer if not NULL
  *
- * @buffer: pointer to the memory block that should be freed
- * @buffer_ptr: a pointer used by getline to index the buffer which should
- * reset to null after freeing the buffer
- * @current_buffer_size: the current size of buffer
- * @bytes_read: number of bytes read from stdin
+ * Return: s
  */
-void free_buffer(char **buffer,
-		UNUSED char **buffer_ptr,
-		UNUSED size_t *current_buffer_size,
-		UNUSED size_t *bytes_read)
+int _getline(info_t *info, char **ptr, size_t *length)
 {
-	free(*buffer);
-	*buffer = NULL;
-	*buffer_ptr = NULL;
-	*current_buffer_size = 0;
-	*bytes_read = 0;
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p) /* MALLOC FAILURE! */
+		return (p ? free(p), -1 : -1);
+
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+
+	s += k - i;
+	i = k;
+	p = new_p;
+
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
 }
 
-
+/**
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
