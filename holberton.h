5e1b25f3c644ed1da64f7be765fecb275d2b6993
Mohamed Ebeid <125565159+ebeidoo@users.noreply.github.com>
2023-05-26 11:22:29 +0300

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -92,7 +92,7 @@ typedef struct builtin_s
 	int (*f)(data_shell *datash);
 } builtin_t;
 
-/* aux_lists.c */
+/* lists.c */
 sep_list *add_sep_node_end(sep_list **head, char sep);
 void free_sep_list(sep_list **head);
 line_list *add_line_node_end(line_list **head, char *line);
