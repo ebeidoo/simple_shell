1637efca72abab029ea346ec93d4e787a292e16a
Mohamed Ebeid <125565159+ebeidoo@users.noreply.github.com>
2023-05-26 22:55:51 +0300

    Update get_help.c

--- get_help.c
+++ get_help.c
@@ -9,21 +9,21 @@ int get_help(data_shell *datash)
 {
 
 	if (datash->args[1] == 0)
-		 help_general();
+		help_general();
 	else if (_strcmp(datash->args[1], "setenv") == 0)
-		 help_setenv();
+		help_setenv();
 	else if (_strcmp(datash->args[1], "env") == 0)
-		 help_env();
+		help_env();
 	else if (_strcmp(datash->args[1], "unsetenv") == 0)
-		 help_unsetenv();
+		help_unsetenv();
 	else if (_strcmp(datash->args[1], "help") == 0)
-		 help();
+		help();
 	else if (_strcmp(datash->args[1], "exit") == 0)
-		 help_exit();
+		help_exit();
 	else if (_strcmp(datash->args[1], "cd") == 0)
-		 help_cd();
+		help_cd();
 	else if (_strcmp(datash->args[1], "alias") == 0)
-		 help_alias();
+		help_alias();
 	else
 		write(STDERR_FILENO, datash->args[0],
 		      _strlen(datash->args[0]));
